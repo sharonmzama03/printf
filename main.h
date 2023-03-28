87f1c690f4c4484647422707e26109ccf1bbccdf
Zakaria Abdul-Rasheed <88566627+arzakaria1@users.noreply.github.com>
2023-03-28 23:24:40 +0000

    Update main.h

--- main.h
+++ main.h
@@ -34,7 +34,7 @@ struct fmt
  * typedef struct fmt fmt_t - Struct format
  *
  * @fmt: The format.
- * @fm_t: The function associated.
+ * @fm_t: The associated Functions.
  */
 typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
