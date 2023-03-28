a6bf6d9b11b1c73ddca1869824c40ca904e530b0
Zakaria Abdul-Rasheed <88566627+arzakaria1@users.noreply.github.com>
2023-03-28 23:30:16 +0000

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /******************* PRINT POINTER *******************/
 /**
- * print_pointer - Prints the value of a pointer variable
+ * print_pointer - displays the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
